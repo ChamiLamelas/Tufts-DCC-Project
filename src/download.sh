94220dd2f71102572b85b49a66d7cb33fe980d37
ChamiLamelas <chami.lamelas@gmail.com>
2023-03-08 08:25:42 -0500

    add download script

---
+++ src/download.sh
@@ -0,0 +1,9 @@
+# max is 144
+
+var=$(($1-1))
+
+for i in $( eval echo {0..$var} ); do wget -c --retry-connrefused --tries=0 --timeout=50 http://alitrip.oss-cn-zhangjiakou.aliyuncs.com/TraceData/MSCallGraph/MSCallGraph_${i}.tar.gz; done
+
+for i in $( eval echo {0..$var} ); do mv MSCallGraph_${i}.tar.gz ../data; done
+
+for i in $( eval echo {0..$var} ); do tar -xf ../data/MSCallGraph_${i}.tar.gz -C ../data; done
