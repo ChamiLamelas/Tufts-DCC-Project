114a99da2c45687e1c70be5373ecdcd522c0e66c
Chami <chami.lamelas@gmail.com>
2023-03-08 20:10:41 -0600

    separated data download

--- setup/data.sh
+++ setup/data.sh
@@ -1,9 +1,5 @@
-# max is 144
-
-var=$(($1-1))
-
+var=144
 for i in $( eval echo {0..$var} ); do wget -c --retry-connrefused --tries=0 --timeout=50 http://alitrip.oss-cn-zhangjiakou.aliyuncs.com/TraceData/MSCallGraph/MSCallGraph_${i}.tar.gz; done
-
 for i in $( eval echo {0..$var} ); do mv MSCallGraph_${i}.tar.gz ../data; done
-
 for i in $( eval echo {0..$var} ); do tar -xf ../data/MSCallGraph_${i}.tar.gz -C ../data; done
+
--- setup/setup2.sh
+++ setup/setup2.sh
@@ -7,5 +7,4 @@ pip3 install pandas
 pip3 install matplotlib
 pip3 install scipy
 pip3 install networkx
-git config --global user.name Chami Lamelas 
-git config --global user.email chami.lamelas@gmail.com
+
