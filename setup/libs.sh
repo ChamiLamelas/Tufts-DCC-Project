71112b009ee3c6842dd8065db04a59a7a0dc2b91
slamel01 <slamel01@node0.dcc-project.tuftscc-pg0.wisc.cloudlab.us>
2023-03-08 19:49:47 -0600

    added libraries setup script

---
+++ setup/libs.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+sudo apt-get update
+sudo apt -y install python3.10
+sudo apt install python3-pip
+pip3 install pandas
+pip3 install matplotlib
+pip3 install scipy
+pip3 install networkx
