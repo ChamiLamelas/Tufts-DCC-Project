324d4bfb5cec3364b61b383b213541111ce5eebb
slamel01 <slamel01@node0.dcc2.tuftscc-pg0.wisc.cloudlab.us>
2023-04-20 11:19:38 -0500

    fixed up download script on 2nd expt

--- setup/download.py
+++ setup/download.py
@@ -3,17 +3,22 @@ import multiprocessing as mp
 import subprocess as sp
 import sys
 import os
-
+import time
+from datetime import timedelta
+from math import ceil
 
 def downloader(idx):
+    print(f"Starting download for file {idx}", file=sys.stderr)
     sp.run(["wget", "-c", "--retry-connrefused", "--tries=0", "--timeout=50",
-           f"http://alitrip.oss-cn-zhangjiakou.aliyuncs.com/TraceData/MSCallGraph/MSCallGraph_${idx}.tar.gz"])
-    sp.run(["tar", "-xf", f"MSCallGraph_${idx}.tar.gz", "-C", "../data"])
-    os.remove(f"MSCallGraph_${idx}.tar.gz")
+           f"http://alitrip.oss-cn-zhangjiakou.aliyuncs.com/TraceData/MSCallGraph/MSCallGraph_{idx}.tar.gz"], stdout=sp.PIPE, stderr=sp.PIPE)
     print(f"Completed download for file {idx}", file=sys.stderr)
+    sp.run(["tar", "-xf", f"MSCallGraph_{idx}.tar.gz", "-C", "../data"], stdout=sp.PIPE, stderr=sp.PIPE)
+    os.remove(f"MSCallGraph_{idx}.tar.gz")
+    print(f"Extracted zip for file {idx}", file=sys.stderr)
 
 
 def download(start_idx, end_idx):
+    ti = time.time()
     children = list()
     for idx in range(start_idx, end_idx + 1):
         child = mp.Process(target=downloader, args=(idx,))
@@ -21,7 +26,8 @@ def download(start_idx, end_idx):
         children.append(child)
     for child in children:
         child.join()
-    print(f"All {end_idx - start_idx + 1} downloads finished")
+    tf = time.time()
+    print(f"All {end_idx - start_idx + 1} downloads finished in {str(timedelta(seconds=ceil(tf-ti)))}")
 
 
 def main():
