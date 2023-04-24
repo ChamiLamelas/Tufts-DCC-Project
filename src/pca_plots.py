63a2869ae803b467fccaf06557190db9dc0eb00f
Chami Lamelas <Swaminathan.Lamelas@tufts.edu>
2023-04-23 21:13:44 -0500

    linda: plot sactter for each service and together

--- src/pca_plots.py
+++ src/pca_plots.py
@@ -9,9 +9,10 @@ def plot_eigenvecs_clusters(file_id):
     embed_path = os.path.join(c.RESULT_FOLDER, c.EMBEDDINGS)
     file_name = os.path.join(embed_path, 'MSCallGraph_'+str(file_id)+'.csv.pca.json')
     eigen_vecs = json.load(open(file_name, 'r'))
+    print(f"... ploting scatters for {len(eigen_vecs)} microservices ...")
 
-    # plot clusters
-    path = os.path.join(embed_path, str(file_id)+'_pca.png')
+    # plot clusters in one fig
+    path = os.path.join(embed_path, 'MSCallGraph_'+str(file_id)+'_pca.png')
     c.prep_path(path)
     cmap = plt.get_cmap('gnuplot')
     colors = [cmap(i) for i in np.linspace(0, 1, len(eigen_vecs))]
@@ -19,16 +20,22 @@ def plot_eigenvecs_clusters(file_id):
     for i, k in enumerate(eigen_vecs):
         plt.scatter(eigen_vecs[k][0], eigen_vecs[k][1], s=1, c=colors[i], label=k)
     plt.legend(loc='best', prop={'size': 8})
-    plt.title("PCA Eigenvectors Clustered By Service")
+    plt.title("PCA Eigenvectors Colored By Service")
     plt.grid()
     plt.savefig(path)
 
-def main():
-    # plot_concurrency_histogram()
-    # plot_depth_histogram()
-    # lookat_concurrencies()
-    plot_eigenvecs_clusters(128)
+    # plot clusters for each service
+    for k in eigen_vecs:
+        path = os.path.join(embed_path, 'microservice_'+k+'_pca.png')
+        c.prep_path(path)
+        plt.figure()
+        plt.scatter(eigen_vecs[k][0], eigen_vecs[k][1], s=1)
+        plt.title("PCA Eigenvector Clusters for Microservice "+k)
+        plt.grid()
+        plt.savefig(path)
 
+def main():
+    plot_eigenvecs_clusters(128)
 
 if __name__ == '__main__':
     main()
